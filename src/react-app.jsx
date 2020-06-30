f1da3b64571b7584b67ac43f49cc97d4ea2cae5d
richard1230 <yongjinhu1230@gmail.com>
2020-06-30 16:21:33 +0800

    基本渲染 使用useEffect useRef，增加了初始化数据

--- src/react-app.jsx
+++ src/react-app.jsx
@@ -3,6 +3,7 @@ import {ReactEcharts} from "./react-echarts";
 
 export function ReactApp() {
 
+
   return(
     <div>
       <h1>我来在react里面使用 Echarts</h1>
--- src/react-echarts.jsx
+++ src/react-echarts.jsx
@@ -4,16 +4,20 @@ import echarts from "echarts"
 export function ReactEcharts() {
   //怎么获取div,初始值为null
   const container = useRef(null)
-
+  const chart = useRef(null)
 // 第一次点击进来渲染图表
   useEffect(()=>{
     // 获取div的值,用current
     console.log(container.current);
+    //初始化return里面的
+    const width = document.documentElement.clientWidth
+    container.current.style.width = `${width - 20}px`
+    container.current.style.height = `${(width - 20) * 1.2}px`
+    chart.current= echarts.init(container.current,'dark')
 
   },[])
   return(
-    <div ref={container}>
-      hi
-    </div>
+    <div ref={container}/>
+
   )
 }
