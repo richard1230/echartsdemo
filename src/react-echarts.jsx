6af436f0702daac9b6dd76c8e4da39cafaca0f96
richard1230 <yongjinhu1230@gmail.com>
2020-06-30 16:01:29 +0800

    基本渲染 使用useEffect useRef

--- src/react-echarts.jsx
+++ src/react-echarts.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
 import echarts from "echarts"
 
 export function ReactEcharts() {
+  //怎么获取div,初始值为null
+  const container = useRef(null)
 
+// 第一次点击进来渲染图表
+  useEffect(()=>{
+    // 获取div的值,用current
+    console.log(container.current);
+
+  },[])
   return(
-    <div>
+    <div ref={container}>
       hi
     </div>
   )
